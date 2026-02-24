811d2ce744a10272cdcfc36feb18eab814d55402
Jean-Eudes <eudes.agbre@gmail.com>
2026-02-24 12:46:54 +0100

    Updated api routings if habits and users models

--- app/routes/v1/users.py
+++ app/routes/v1/users.py
@@ -11,16 +11,23 @@ router = APIRouter()
 # def user_check():
 #     return {"message": "User endpoint is working!"}
 
-@router.post("/", response_model=schemas.users.UserResponse)
-def create_user(user: schemas.users.UserCreate, db: Session = Depends(configs.get_db)):
-    db_user = db.query(models.User).filter((models.User.username == user.username) | (models.User.email == user.email)).first()
+@router.post("/", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+
+    db_user = db.query(User).filter((User.username == user.username) | (User.email == user.email)).first()
     if db_user:
-        raise HTTPException(status_code=400, detail="Username or email already registered")
+        raise HTTPException(status_code=400, detail="Username or email already registered! Please choose another one.")
     
     hashed_password = user.password + "notreallyhashed"  # Placeholder for hashing
-    new_user = models.User(username=user.username, email=user.email, hashed_password=hashed_password)
+    new_user = User(username=user.username, email=user.email, hashed_password=hashed_password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
 
+@router.get("/{user_id}", response_model=UserResponse)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
