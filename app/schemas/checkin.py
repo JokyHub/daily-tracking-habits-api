eb69cb6fe3b16e632455a0d519ddb06cd27a02b7
Jean-Eudes <eudes.agbre@gmail.com>
2026-02-24 15:37:33 +0100

    Updated models; API is functional

--- app/schemas/checkin.py
+++ app/schemas/checkin.py
@@ -3,14 +3,16 @@ from datetime import datetime
 
 class CheckinBase(BaseModel):
     habit_id: int
-    date: datetime
+    user_id: int
+
 class CheckinCreate(CheckinBase):
     pass
 
 class CheckinResponse(CheckinBase):
-    id: int
-    habit_id: int
-    date: datetime
+    # id: int
+    # habit_id: int
+    # user_id: int
+    checkin_date: datetime
 
     class Config:
         from_attributes = True
