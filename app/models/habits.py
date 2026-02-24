33a1a75224710686a6a795336cf80f76b5b4284d
Jean-Eudes <eudes.agbre@gmail.com>
2026-02-24 11:44:09 +0100

    Updated the models checkin and habits

---
+++ app/models/habits.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from app.configs.database import Base
+from datetime import datetime
+from sqlalchemy.orm import relationship
+
+class Habit(Base):
+    __tablename__ = "habits"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=False, nullable=False)
+    description = Column(String, nullable=True)
+
+    user_id = Column(Integer, nullable=False)  # Foreign key to users.id
+
+    current_streak = Column(Integer, default=0, nullable=False)
+    longest_streak = Column(Integer, default=0, nullable=False)
+    last_checkin_date = Column(DateTime, nullable=True)
+
+    user = relationship("User")
+
+
